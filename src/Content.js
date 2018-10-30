165dcca4db89a4e22cd38332bb81dea28b2e5600
jiasheng_ou <oujia6014@foxmail.com>
2018-10-30 15:21:47 +0800

    实现React-redux

--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import ThemeSwitch from './ThemeSwitch'
 import { connect } from './react-redux'
 class Content extends Component {
-    static contextTypes = {
+    static propTypes = {
         themeColor:PropTypes.string
     }
     render(){
@@ -22,31 +22,3 @@ const mapStateToProps = (state) => {
   }
 Content = connect(mapStateToProps)(Content)
 export default Content
-
-// class Content extends Component {
-//     static contextTypes = {
-//         store:PropTypes.object
-//     }
-//     constructor(){
-//         super()
-//         this.state = {themeColor:''}
-//     }
-//     componentWillMount(){
-//         const {store} = this.context
-//         this._updateThemeColor()
-//         store.subscribe( () => this._updateThemeColor() )
-//     }
-//     _updateThemeColor(){
-//         const { store } = this.context
-//         const state = store.getState()
-//         this.setState({themeColor:state.themeColor})
-//     }
-//     render(){
-//         return (
-//             <div>
-//                 <p style={{color:this.state.themeColor}}>React.js 内容</p>
-//                 <ThemeSwitch />
-//             </div>
-//         )
-//     }
-// }
